7ca22c46f7b838f58b467aa1f1ccdc3102c9f0d0
Juan Sebastian Ramirez <juan.ramirez@doublevpartners.com>
2021-07-21 01:06:18 -0500

    Multilinea, derstructuracion, O.Propagacion, D.Variables, Constantes

--- src/es6/index.js
+++ src/es6/index.js
@@ -30,3 +30,67 @@ console.log(phrase);
 //ECMAScript6
 let phraseES6 = `${hello} ${world}`;
 console.log(phraseES6);
+
+//====================Multilinea
+//Antes de ECMAScript6
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit \n'
+    +'. Praesent in erat elementum, pulvinar dolor ac, faucibus mi.\n'
+    +'Vestibulum suscipit ante vitae nunc dignissim pellentesque.\n'
+    +'Vivamus et nisl suscipit, feugiat dui quis, ultrices erat.';
+console.log(lorem);
+
+//ECMAScript6
+let loremES6 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Praesent in erat elementum, pulvinar dolor ac, faucibus mi. Vestibulum 
+    suscipit ante vitae nunc dignissim pellentesque. Vivamus et nisl suscipit, 
+    feugiat dui quis, ultrices erat.`;
+
+console.log(loremES6);
+
+//====================Destructuracion de elementos
+let person = {
+    'name': 'Juan',
+    'age': 21,
+    'country': 'COL'
+}
+
+console.log(person.name, person.age, person.country);
+
+
+//====================Operador de Propagacion ...
+console.log(name, age, country);
+
+let team1 = [
+    'Oscar', 'Ricardo', 'Omar'
+];
+
+let team2 = [
+    'Manuela', 'Patricia', 'Caamila'
+];
+
+let education = ['David',...team1, ...team2];
+
+console.log(education);
+
+//====================Declaracion de variables
+//Antes de ECMAScript6
+var hola = 'hola'; //varibale duisponible de forma global
+
+{
+    var global = 'Global Var'; //La variable queda accesible por todo el codigo
+}
+
+{
+    let globalLet = 'global Let';//la variable solo se puede acceder por este bloque de coidgo
+    console.log(globalLet);
+}
+
+console.log(global);
+//console.log(globalLet);//return Error
+
+//====================Constantes
+const a = 'b';
+/*
+a = 'a'; //return error
+*/
+console.log(a);
