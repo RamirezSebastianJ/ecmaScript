f33381bd1cfc8c0c847aa41c922f375529652a4c
Juan Sebastian Ramirez <juan.ramirez@doublevpartners.com>
2021-07-21 01:49:36 -0500

    ES7 includes y Potencias

---
+++ src/es7/index.js
@@ -0,0 +1,19 @@
+//====================Includes
+let numbers = [1,2,5,3,36,8];
+if(numbers.includes(5)){
+    console.log('Si se encuentra el valor 5');
+}else{
+    console.log('no se encuentra el valor 7');
+}
+
+if(numbers.includes(4)){
+    console.log('Si se encuentra el valor 4');
+}else{
+    console.log('no se encuentra el valor 4');
+}
+
+//====================Potencias
+let base= 4;
+let exponent = 3;
+let result = base**exponent;
+console.log(result);
