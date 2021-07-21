b642b6f54b88fa7e7a73dae4a095126311813db3
Juan Sebastian Ramirez <juan.ramirez@doublevpartners.com>
2021-07-21 02:10:10 -0500

    async await

--- src/es8/index.js
+++ src/es8/index.js
@@ -24,3 +24,33 @@ console.log(values.length);
 const strings = 'hellow';
 console.log(strings.padStart(7, 'Hi')); //Tratta de agregar la nueva cadena al inicio sin pasar la longitud de 7
 console.log(strings.padEnd(7, '------'));//Tratta de agregar la nueva cadena al final sin pasar la longitud de 7
+
+
+//====================Async Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true) 
+            ? setTimeout(() => resolve(helloWorld), 3000)
+            : reject(new Error('Test Error'));
+        
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+//Forma correcta de ejecucion
+const anotherFunc = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunc();
